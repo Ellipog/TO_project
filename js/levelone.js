dd3d628e0a0939716e1c28137bd387bffb99beab
Ellipog <45790498+Ellipog@users.noreply.github.com>
2022-11-15 13:00:04 +0100

    blur

--- js/levelone.js
+++ js/levelone.js
@@ -1,144 +1,147 @@
 var jsonData = {
     "module": {
-      "number": 1,
-      "name": "Level-one",
-      "questions": 10,
-      "revision": "HUH"
-    },
-    "questions": [
-      {
         "number": 1,
-        "question": "What food is this?",
-        "answers": [
-          "Banana",
-          "Apple",
-          "Strawberry",
-          "Blueberry"
-        ],
-        "correct_answer": 0,
-        "hint":"Yellow Fruit"
-      },
-      {
-        "number": 2,
-        "question": "What food is this?",
-        "answers": [
-          "Pancake",
-          "Waffle",
-          "Scones",
-          "Nuggets"
-        ],
-        "correct_answer": 1,
-        "hint":"Heart shaped"
-      },
-          {
-        "number": 3,
-        "question": "What food is this?",
-        "answers": [
-          "Taco",
-          "Steak",
-          "Pie",
-          "Mac'N Cheese"
-        ],
-        "correct_answer": 2,
-        "hint":"Comes in a casserole"
-      },
-          {
-        "number": 4,
-        "question": "What food is this?",
-        "answers": [
-          "Corn Flakes",
-          "Mushrooms",
-          "Ceasar Salad",
-          "Yoghurt"
-        ],
-        "correct_answer": 2,
-        "hint":"A type of salad"
-      },
-          {
-        "number": 5,
-        "question": "What food is this?",
-        "answers": [
-          "Tater Tots",
-          "Bread",
-          "Fajitas",
-          "Salmon"
-        ],
-        "correct_answer": 3,
-        "hint": "Comes from the sea"
-      },
-          {
-        "number": 6,
-        "question": "What food is this?",
-        "answers": [
-          "P&J Sandwich",
-          "Enchiladas",
-          "Smores",
-          "Chicken wings"
-        ],
-        "correct_answer": 2,
-        "hint": "Dessert"
-      },
-          {
-        "number": 7,
-        "question": "What food is this?",
-        "answers": [
-          "Lasagne",
-          "BBQ Ribs",
-          "Apple Pie",
-          "Chocolate Chip Cookies"
-        ],
-        "correct_answer": 3,
-        "hint":"Dessert"
-      },
-          {
-        "number": 8,
-        "question": "What food is this?",
-        "answers": [
-          "Beef Stroganof",
-          "Spagetthi and Meatballs",
-          "Chicken and Rice",
-          "Pizza"
-        ],
-        "correct_answer": 0,
-        "hint":"Pasta and Meat"
-      },
-          {
-        "number": 9,
-        "question": "What food is this?",
-        "answers": [
-          "Nachos",
-          "Hot Dogs",
-          "Cheese Burger",
-          "Turkey"
-        ],
-        "correct_answer": 1,
-        "hint":"Sausage"
-      },
-          {
-        "number": 10,
-        "question": "What food is this?",
-        "answers": [
-          "Spaghetti and meatballs",
-          "Fried chicken",
-          "Beef stir fry",
-          "Burritos"
-        ],
-        "correct_answer": 3,
-        "hint":"looks like a taco"
-      }
-      
+        "name": "Level-one",
+        "questions": 10,
+        "revision": "HUH"
+    },
+    "questions": [{
+            "number": 1,
+            "question": "What food is this?",
+            "answers": [
+                "Banana",
+                "Apple",
+                "Strawberry",
+                "Blueberry"
+            ],
+            "correct_answer": 0,
+            "hint": "Yellow Fruit"
+        },
+        {
+            "number": 2,
+            "question": "What food is this?",
+            "answers": [
+                "Pancake",
+                "Waffle",
+                "Scones",
+                "Nuggets"
+            ],
+            "correct_answer": 1,
+            "hint": "Heart shaped"
+        },
+        {
+            "number": 3,
+            "question": "What food is this?",
+            "answers": [
+                "Taco",
+                "Steak",
+                "Pie",
+                "Mac'N Cheese"
+            ],
+            "correct_answer": 2,
+            "hint": "Comes in a casserole"
+        },
+        {
+            "number": 4,
+            "question": "What food is this?",
+            "answers": [
+                "Corn Flakes",
+                "Mushrooms",
+                "Ceasar Salad",
+                "Yoghurt"
+            ],
+            "correct_answer": 2,
+            "hint": "A type of salad"
+        },
+        {
+            "number": 5,
+            "question": "What food is this?",
+            "answers": [
+                "Tater Tots",
+                "Bread",
+                "Fajitas",
+                "Salmon"
+            ],
+            "correct_answer": 3,
+            "hint": "Comes from the sea"
+        },
+        {
+            "number": 6,
+            "question": "What food is this?",
+            "answers": [
+                "P&J Sandwich",
+                "Enchiladas",
+                "Smores",
+                "Chicken wings"
+            ],
+            "correct_answer": 2,
+            "hint": "Dessert"
+        },
+        {
+            "number": 7,
+            "question": "What food is this?",
+            "answers": [
+                "Lasagne",
+                "BBQ Ribs",
+                "Apple Pie",
+                "Chocolate Chip Cookies"
+            ],
+            "correct_answer": 3,
+            "hint": "Dessert"
+        },
+        {
+            "number": 8,
+            "question": "What food is this?",
+            "answers": [
+                "Beef Stroganof",
+                "Spagetthi and Meatballs",
+                "Chicken and Rice",
+                "Pizza"
+            ],
+            "correct_answer": 0,
+            "hint": "Pasta and Meat"
+        },
+        {
+            "number": 9,
+            "question": "What food is this?",
+            "answers": [
+                "Nachos",
+                "Hot Dogs",
+                "Cheese Burger",
+                "Turkey"
+            ],
+            "correct_answer": 1,
+            "hint": "Sausage"
+        },
+        {
+            "number": 10,
+            "question": "What food is this?",
+            "answers": [
+                "Spaghetti and meatballs",
+                "Fried chicken",
+                "Beef stir fry",
+                "Burritos"
+            ],
+            "correct_answer": 3,
+            "hint": "looks like a taco"
+        }
+
     ]
-  }
+}
 
-  var count = 0;
+var count = 0;
 
-  answers();
+answers();
 
-  function answer() {
-    answers();
-  }
+function answer() {
+    blur()
+}
 
-  function answers() {
+function blur() {
+    document.getElementById("blur").style.filter = "blur(5px)";
+}
+
+function answers() {
     var question = jsonData.questions[count];
     var answer1 = question.answers[0];
     var answer2 = question.answers[1];
@@ -149,5 +152,5 @@ var jsonData = {
     document.getElementById("answer2").innerHTML = answer2;
     document.getElementById("answer3").innerHTML = answer3;
     document.getElementById("answer4").innerHTML = answer4;
-    
-  }
+
+}
