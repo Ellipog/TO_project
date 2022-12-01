6274c1f51de5c404b6cdb8447b0c038309b08fa3
thammer <tobias@hammerhome.no>
2022-12-01 12:54:12 +0100

    ny js leveler

---
+++ docs/js/levelfour.js
@@ -0,0 +1,141 @@
+const jsonData = {
+    "module": {
+        "number": 1,
+        "name": "Level-one",
+        "images": 10,
+        "questions": 10,
+        "revision": "HUH"
+    },
+    "questions": [{
+            "number": 1,
+            "question": "What is this?",
+            "images": "images/sink.png",
+            "answers": [
+                "Fork",
+                "Glass",
+                "Blender",
+                "Sink"
+            ],
+            "correct_answer": 3,
+            "hint": "la det synke inn"
+        },
+        {
+            "number": 2,
+            "question": "What is this?",
+            "images": "images/knife.png",
+            "answers": [
+                "Bowl",
+                "Dishwasher",
+                "Knife",
+                "Pokemon"
+            ],
+            "correct_answer": 2,
+            "hint": "Ruter"
+        },
+        {
+            "number": 3,
+            "question": "What is this?",
+            "images": "images/mug.png",
+            "answers": [
+                "A Mug",
+                "Boiler",
+                "Bread Knife",
+                "Spoon"
+            ],
+            "correct_answer": 0,
+            "hint": "Comes in a casserole"
+        },
+        {
+            "number": 4,
+            "question": "What is this??",
+            "images": "images/whisk.png",
+            "answers": [
+                "Coffee maker",
+                "Waffle Iron",
+                "Whisk",
+                "Frying Pan"
+            ],
+            "correct_answer": 2,
+            "hint": "A type of salad"
+        },
+        {
+            "number": 5,
+            "question": "What is this?",
+            "images": "images/freezer.png",
+            "answers": [
+                "Toaster",
+                "Freezer",
+                "Can Opener",
+                "Measuring Jug"
+            ],
+            "correct_answer": 1,
+            "hint": "Comes from the sea"
+        },
+        {
+            "number": 6,
+            "question": "What is this?",
+            "images": "images/spatula.png",
+            "answers": [
+                "Spatula",
+                "Corkskrew",
+                "Oven Gloves",
+                "Aluminium foil"
+            ],
+            "correct_answer": 0,
+            "hint": "Dessert"
+        },
+        {
+            "number": 7,
+            "question": "What is this?",
+            "images": "images/vegetablepeeler.png",
+            "answers": [
+                "Fridge",
+                "Kitchen scales",
+                "Pizza Cutter",
+                "Vegetable peeler"
+            ],
+            "correct_answer": 3,
+            "hint": "Dessert"
+        },
+        {
+            "number": 8,
+            "question": "Hvilket kjøkken-redskap er dette?",
+            "images": "images/teaspoon.png",
+            "answers": [
+                "Microwave",
+                "Plastic wrap",
+                "Teaspoon",
+                "Kettle"
+            ],
+            "correct_answer": 2,
+            "hint": "Pasta and Meat"
+        },
+        {
+            "number": 9,
+            "question": "Hvilket kjøkken-redskap er dette?",
+            "images": "images/strainer.png",
+            "answers": [
+                "Casserole",
+                "Kitchen scissors",
+                "Strainer",
+                "Laddle"
+            ],
+            "correct_answer": 2,
+            "hint": "Sausage"
+        },
+        {
+            "number": 10,
+            "question": "Hvilket kjøkken-redskap er dette?",
+            "images": "images/bottles.png",
+            "answers": [
+                "Bottles",
+                "Thermos",
+                "Cutting board",
+                "Napkin"
+            ],
+            "correct_answer": 0,
+            "hint": "looks like a taco"
+        }
+
+    ]
+}
