43dcd0d58875aa2dece769b184e5b34069ea17fc
thammer <tobias@hammerhome.no>
2022-12-05 23:38:22 +0100

    levelfourstart

--- docs/js/levelfour.js
+++ docs/js/levelfour.js
@@ -8,124 +8,334 @@ const jsonData = {
     },
     "questions": [{
             "number": 1,
-            "images": "images/sink.png",
+            "images": "images/caps.png",
             "answers": [
-                "Fork",
-                "Glass",
-                "Blender",
-                "Sink"
+                "Caps",
+                "Boxer shorts",
+                "Tie",
+                "Gloves"
             ],
-            "correct_answer": 3,
-            "hint": "la det synke inn"
+            "correct_answer": 0,
+            "hint": "Norsk-engelsk"
         },
         {
             "number": 2,
-            "images": "images/knife.png",
+            "images": "images/sneakers.png",
             "answers": [
-                "Bowl",
-                "Dishwasher",
-                "Knife",
-                "Pokemon"
+                "Mittons",
+                "Bowtie",
+                "Tights",
+                "Sneakers"
             ],
-            "correct_answer": 2,
-            "hint": "Ruter"
+            "correct_answer": 3,
+            "hint": "Flest bokstaver"
         },
         {
             "number": 3,
-            "images": "images/mug.png",
+            "images": "images/shirt.png",
             "answers": [
-                "A Mug",
-                "Boiler",
-                "Bread Knife",
-                "Spoon"
+                "Tank top",
+                "Shirt",
+                "Vest",
+                "Scarf"
             ],
-            "correct_answer": 0,
-            "hint": "Comes in a casserole"
+            "correct_answer": 1,
+            "hint": "5 bokstaver"
         },
         {
             "number": 4,
-            "images": "images/whisk.png",
+            "images": "images/socks.png",
             "answers": [
-                "Coffee maker",
-                "Waffle Iron",
-                "Whisk",
-                "Frying Pan"
+                "Socks",
+                "Belt",
+                "Swim suit",
+                "Suspenders"
             ],
-            "correct_answer": 2,
-            "hint": "A type of salad"
+            "correct_answer": 0,
+            "hint": "Likner norsk"
         },
         {
             "number": 5,
-            "images": "images/freezer.png",
+            "images": "images/pants.png",
             "answers": [
-                "Toaster",
-                "Freezer",
-                "Can Opener",
-                "Measuring Jug"
+                "Hat",
+                "Rain coat",
+                "Pants",
+                "Boots"
             ],
-            "correct_answer": 1,
-            "hint": "Comes from the sea"
+            "correct_answer": 2,
+            "hint": "Ikke Hat eller Boots"
         },
         {
             "number": 6,
-            "images": "images/spatula.png",
+            "images": "images/jacket.png",
             "answers": [
-                "Spatula",
-                "Corkskrew",
-                "Oven Gloves",
-                "Aluminium foil"
+                "Sunglasses",
+                "Hoodie",
+                "Sweatpants",
+                "Jacket"
             ],
-            "correct_answer": 0,
-            "hint": "Dessert"
+            "correct_answer": 3,
+            "hint": "C er i ordet"
         },
         {
             "number": 7,
-            "images": "images/vegetablepeeler.png",
+            "images": "images/sweater.png",
             "answers": [
-                "Fridge",
-                "Kitchen scales",
-                "Pizza Cutter",
-                "Vegetable peeler"
+                "Track suit",
+                "High heels",
+                "Sweater",
+                "Skirt"
             ],
-            "correct_answer": 3,
-            "hint": "Dessert"
+            "correct_answer": 2,
+            "hint": "Begynner på S"
         },
         {
             "number": 8,
-            "images": "images/teaspoon.png",
+            "images": "images/shorts.png",
             "answers": [
-                "Microwave",
-                "Plastic wrap",
-                "Teaspoon",
-                "Kettle"
+                "Suit",
+                "Shorts",
+                "Polo Shirt",
+                "Singlet"
             ],
-            "correct_answer": 2,
-            "hint": "Pasta and Meat"
+            "correct_answer": 1,
+            "hint": "Norsk-engelsk"
         },
         {
             "number": 9,
-            "images": "images/strainer.png",
+            "images": "images/Blazer.png",
             "answers": [
-                "Casserole",
-                "Kitchen scissors",
-                "Strainer",
-                "Laddle"
+                "Sneakers",
+                "Blazer",
+                "Jeans",
+                "Flip flops"
             ],
             "correct_answer": 2,
-            "hint": "Sausage"
+            "hint": "Et ord"
         },
         {
             "number": 10,
-            "images": "images/bottles.png",
+            "images": "images/dress.png",
             "answers": [
-                "Bottles",
-                "Thermos",
-                "Cutting board",
-                "Napkin"
+                "Gym clothes",
+                "Blouse",
+                "Pajamas",
+                "Dress"
             ],
-            "correct_answer": 0,
-            "hint": "looks like a taco"
+            "correct_answer": 3,
+            "hint": "Også et norsk ord"
         }
 
     ]
+}
+
+
+var count = 0;
+var frames1 = 0;
+var frames2 = 0;
+var frames3 = 0;
+var progress = 0;
+var redProgress = 0;
+var cor = 0;
+var wrong = 0;
+
+
+function hint() {
+    count--;
+    var question = jsonData.questions[count];
+    var hint = question.hint;
+    console.log(hint)
+    count++;
+    document.getElementById("hint").innerHTML = hint; 
+}
+
+
+
+nextAnswers();
+
+function answer(answer) {
+    count--;
+    var question = jsonData.questions[count];
+    var correct = question.correct_answer;
+    switch (answer) {
+        case 0:
+            if (correct === 0) {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Riktig";
+                count++;
+                cor++;
+                progressBar();
+            } else {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Feil";
+                count++;
+                wrong++;
+                failProgressBar();
+            }
+            break;
+        case 1:
+            if (correct === 1) {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Riktig";
+                count++;
+                cor++;
+                progressBar();
+            } else {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Feil";
+                count++;
+                wrong++;
+                failProgressBar();
+            }
+            break;
+        case 2:
+            if (correct === 2) {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Riktig";
+                count++;
+                cor++;
+                progressBar();
+            } else {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Feil";
+                count++;
+                wrong++;
+                failProgressBar();
+            }
+            break;
+        case 3:
+            if (correct === 3) {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Riktig";
+                count++;
+                cor++;
+                progressBar();
+            } else {
+                resultsButton();
+                document.getElementById("nextText").textContent = "Feil";
+                count++;
+                wrong++;
+                failProgressBar();
+            }
+            break;
+        default:
+    }
+    blur();
+}
+
+function resultsButton() {
+    if (count === 9) {
+        document.getElementById("nextAnswers").textContent = "Resultater";
+    }
+}
+
+function blur() {
+    if (frames2 == 0) {
+        frames2 = 1;
+        var blurCSS = document.getElementById("blur");
+        var blurAmount = 0;
+        var id = setInterval(frame, 20);
+
+        function frame() {
+            if (blurAmount >= 10) {
+                clearInterval(id);
+                frames2 = 0;
+            } else {
+                blurAmount++;
+                blurCSS.style.filter = "blur(" + blurAmount + "px)";
+            }
+        }
+    }
+    document.getElementById("next").style.display = "flex";
+    document.getElementById("answers").style.display = "none";
+}
+
+function nextAnswers() {
+    if (count === 10) {
+        final();
+    } else {
+        next();
+    }
+}
+
+function next() {
+    var question = jsonData.questions[count];
+    var image = question.images;
+    var correct = question.correct_answer;
+    var answer1 = question.answers[0];
+    var answer2 = question.answers[1];
+    var answer3 = question.answers[2];
+    var answer4 = question.answers[3];
+    const visHint = 'Vis hint';
+    var hint = question.hint;
+    count++;
+    document.getElementById("answer1").innerHTML = answer1;
+    document.getElementById("answer2").innerHTML = answer2;
+    document.getElementById("answer3").innerHTML = answer3;
+    document.getElementById("answer4").innerHTML = answer4;   
+    document.getElementById("image").src = image;
+    document.getElementById("hint").innerHTML = visHint;
+    document.getElementById("blur").style.filter = "blur(0px)";
+    document.getElementById("next").style.display = "none";
+    document.getElementById("answers").style.display = "flex";
+}
+
+function final() {
+    document.getElementById("blur").style.display = "none";
+    document.getElementById("next").style.display = "none";
+    document.getElementById("final").style.display = "flex";
+    document.getElementById("results").innerHTML =
+        "Du fikk \r\n" + "<span class='cor'>" + cor + "</span>" + " riktig svar\r\n" +
+        "<span class='wrong'>" + wrong + "</span>" + " feil svar";
+}
+
+function progressBar() {
+    progress += 10;
+    if (progress >= 9) {
+        document.getElementById("redStatus").style.borderRadius = "0px 0px 0px 0px";
+    }
+    if (frames1 == 0) {
+        frames1 = 1;
+        var status = document.getElementById("status");
+        var statusText = document.getElementById("statusText");
+        var width = progress - 10;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= progress) {
+                clearInterval(id);
+                frames1 = 0;
+            } else {
+                width++;
+                status.style.width = width + "%";
+            }
+        }
+    }
+    statusText.innerHTML = (progress + redProgress) / 10 + " / 10";
+}
+
+function failProgressBar() {
+    redProgress += 10;
+    if (progress >= 9) {
+        document.getElementById("redStatus").style.borderRadius = "0px 0px 0px 0px";
+    }
+    if (frames3 == 0) {
+        frames3 = 1;
+        var redStatus = document.getElementById("redStatus");
+        var statusText = document.getElementById("statusText");
+        var width = redProgress - 10;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= redProgress) {
+                clearInterval(id);
+                frames3 = 0;
+            } else {
+                width++;
+                redStatus.style.width = width + "%";
+            }
+        }
+    }
+    statusText.innerHTML = (progress + redProgress) / 10 + " / 10";
 }
